ee0a0f993ba27e3cfdb888f4c031c0a57c44f548
btsouts <btsouts@gmail.com>
2024-11-08 11:00:12 +0000

    Correction in config.mk.

--- src/config.mk
+++ src/config.mk
@@ -3,8 +3,7 @@
 # Explicitly specify which files to compile
 SOURCES = \
 	main.c \
-	submodule-dir-example/src/batt.c \
-	submodule-dir-example/src/estimation.c
+	submodule-dir-example/src/batt.c
 
 # Options that get passed to the C compiler
 CFLAGS = -Isubmodule-dir-example/src
